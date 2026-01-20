2d316d1cbad380d38df8b0bb1688c01177d846f7
NIYOMUGISHA Obed <obed6@duck.com>
2026-01-20 02:39:39 +0200

    Add init hook to handle database connection and update auth handler

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,9 +1,10 @@
-import type { Handle } from "@sveltejs/kit";
-import { paraglideMiddleware } from "$lib/paraglide/server";
-
-import { auth } from "$lib/auth";
-import { svelteKitHandler } from "better-auth/svelte-kit";
-import { sequence } from "@sveltejs/kit/hooks";
+import type { Handle } from '@sveltejs/kit';
+import { paraglideMiddleware } from '$lib/paraglide/server';
+import { auth } from '$lib/auth';
+import { svelteKitHandler } from 'better-auth/svelte-kit';
+import { sequence } from '@sveltejs/kit/hooks';
+import { connect } from '$lib/db';
+import { building } from '$app/environment';
 
 const handleParaglide: Handle = ({ event, resolve }) =>
   paraglideMiddleware(event.request, ({ request, locale }) => {
@@ -11,25 +12,25 @@ const handleParaglide: Handle = ({ event, resolve }) =>
 
     return resolve(event, {
       transformPageChunk: ({ html }) =>
-        html.replace("%paraglide.lang%", locale),
+        html.replace('%paraglide.lang%', locale),
     });
   });
 
 const handleAuth: Handle = async ({ event, resolve }) => {
-  const fetchedSession = await auth.api.getSession({
+  const session = await auth.api.getSession({
     headers: event.request.headers,
   });
 
-  if (fetchedSession) {
-    const { user, session } = fetchedSession;
-    event.locals.session = session;
-    event.locals.user = user;
-  } else {
-    delete event.locals.session;
-    delete event.locals.user;
+  if (session) {
+    event.locals.session = session.session;
+    event.locals.user = session.user;
   }
+  return svelteKitHandler({ event, resolve, auth, building });
+};
 
-  return svelteKitHandler({ event, resolve, auth });
+export const init = async () => {
+  await connect();
+  console.log('Database connected successfully!');
 };
 
 export const handle = sequence(handleAuth, handleParaglide);
--- src/lib/db.ts
+++ src/lib/db.ts
@@ -3,3 +3,12 @@ import { MongoClient } from 'mongodb';
 
 const client = new MongoClient(env.MONGODB_CONNECTION_STRING);
 export const db = client.db();
+
+export async function connect() {
+  await client.connect();
+}
+
+// disconnect from the database
+// export async function disconnect() {
+//   await client.close();
+// }

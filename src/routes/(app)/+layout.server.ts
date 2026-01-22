fd2a3336a8b89d675f98aede7db236ce4ab9369b
NIYOMUGISHA Obed <obed6@duck.com>
2026-01-22 13:33:16 +0200

    Delete old version

--- src/routes/(app)/+layout.server.ts
+++
@@ -1,9 +0,0 @@
-import { error } from '@sveltejs/kit';
-
-export const load = async ({ locals }) => {
-  const userID = locals.user?.id;
-
-  if (!userID) error(401, 'Un Authorized');
-
-  return { userImage: locals.user?.image };
-};
--- src/routes/(app)/account/+page.server.ts
+++
@@ -1,9 +0,0 @@
-import { error } from '@sveltejs/kit';
-
-export const load = ({ locals }) => {
-  if (!locals.user) error(401, 'Un Authorized');
-
-  const { createdAt, email, name, image } = locals.user;
-
-  return { createdAt, email, name, image };
-};

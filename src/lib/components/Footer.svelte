fd2ead8c9ba0e144cfa7155c2e20637d1ee1f719
NIYOMUGISHA Obed <obed6@duck.com>
2026-04-07 00:13:57 +0200

    Update Footer.svelte

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,4 +1,4 @@
 <footer class="page-shell pb-6 pt-8 text-center text-sm text-[var(--app-text-soft)]">
-  <p>Copyright 2025 NIYOMUGISHA Obed.</p>
+  <p>Copyright {new Date().getFullYear()} NIYOMUGISHA Obed.</p>
   <p>All rights reserved.</p>
 </footer>

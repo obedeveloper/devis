9250d71e4bbd8e66515ef9fd401f7c15bd5247ec
NIYOMUGISHA Obed <obed6@duck.com>
2026-07-28 13:18:49 +0200

    Remove vercel analytics

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,13 +1,6 @@
-import { dev } from '$app/environment';
-import { injectAnalytics } from '@vercel/analytics/sveltekit';
-
 import type { LayoutLoad } from './$types';
 import { entries } from 'idb-keyval';
 import { browser } from '$app/environment';
-import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
-
-injectSpeedInsights();
-injectAnalytics({ mode: dev ? 'development' : 'production' });
 
 export const load: LayoutLoad = async () => ({
 	estimations: browser

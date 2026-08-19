90d486174b0c13eec067132446896af35a484fe2
NIYOMUGISHA Obed <obed6@duck.com>
2026-08-19 13:23:04 +0200

    Add navigation bar to the global layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
+	import { resolve } from '$app/paths';
 
 	let { children } = $props();
 </script>
@@ -8,5 +9,12 @@
 <svelte:head>
 	<link rel="icon" href={favicon} />
 </svelte:head>
+
+<header class="sticky top-0 mbe-6 bg-black/85 py-3 text-white">
+	<nav class="wrapper flex justify-between gap-4 text-lg *:hover:underline sm:justify-start">
+		<a href={resolve('/')}>Home</a>
+		<a href={resolve('/new-quote')}>New Quote</a>
+	</nav>
+</header>
 
 {@render children()}

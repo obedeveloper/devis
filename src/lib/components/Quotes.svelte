d9de39d6218475a2af183ca08c0e4662a1e06a91
NIYOMUGISHA Obed <obed6@duck.com>
2026-08-19 09:37:07 +0200

    Add markup for quotes screen on home page

--- src/lib/components/Quotes.svelte
+++ src/lib/components/Quotes.svelte
@@ -4,13 +4,28 @@
 	const quotes = await getQuotes();
 </script>
 
-<section>
-	{#each quotes as { id } (id)}
-		<!--  -->
+<section
+	class={[quotes.length && 'grid grid-cols-[repeat(auto-fit,minmax(min(320px,100%),420px))] gap-3']}
+>
+	{#each quotes as { id, ...data } (id)}
+		{const { title, description, currency, createdAt } = data}
+		{const date = new Date(createdAt)}
+
+		<article class="rounded bg-black/15 p-3 text-black/80">
+			<h3 class="text-lg font-semibold text-black">{title}</h3>
+			<p>{description}</p>
+
+			<div class="mbs-4 flex justify-between">
+				<span class="font-mono text-black/90 uppercase">{currency}</span>
+				<time datetime={date.toISOString()}>
+					{date.toLocaleDateString(undefined, { day: '2-digit', month: 'short', year: 'numeric' })}
+				</time>
+			</div>
+		</article>
 	{:else}
-		<p>
-			You don't have quotes yet! <a class="underline" href={resolve('/new-quote')}>Create new one</a
-			> to get started.
+		<p class="text-center">
+			You don't have quotes yet!
+			<a class="underline" href={resolve('/new-quote')}>Create new one </a> to get started.
 		</p>
 	{/each}
 </section>

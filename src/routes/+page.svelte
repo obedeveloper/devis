8e415e7b54d106fda72404bff2ab88cd4263c200
NIYOMUGISHA Obed <obed6@duck.com>
2026-08-22 15:02:17 +0200

    feat: add meta description to index page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,6 +7,10 @@
 
 <svelte:head>
 	<title>Devis</title>
+	<meta
+		name="description"
+		content="Create and manage professional quotes in minutes, and export them as polished PDFs. Free for freelancers and small businesses."
+	/>
 </svelte:head>
 
 {#if !user}

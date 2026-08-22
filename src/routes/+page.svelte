eb733bee9d6c6606de9ebda854b542b844bfd0f0
NIYOMUGISHA Obed <obed6@duck.com>
2026-08-22 14:08:05 +0200

    feat: show landing page for signed-out visitors

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import { signIn } from '$lib/auth-client';
+	import Landing from '$lib/components/Landing.svelte';
 	import Quotes from '$lib/components/Quotes.svelte';
 	import { getUser } from '$lib/user.remote';
 	const user = $derived(await getUser());
@@ -9,18 +9,10 @@
 	<title>Devis</title>
 </svelte:head>
 
-<main class={[!user && 'grid min-h-svh place-content-center', 'wrapper']}>
-	{#if !user}
-		<section>
-			<h3 class="mbe-4">You are signed out! Sign in to continue.</h3>
-			<button
-				onclick={() => signIn()}
-				class="mx-auto block rounded bg-black px-5 py-2 text-lg font-semibold text-white transition-colors duration-300 hover:bg-black/85"
-			>
-				Sign in
-			</button>
-		</section>
-	{:else}
+{#if !user}
+	<Landing />
+{:else}
+	<main class="wrapper">
 		<Quotes></Quotes>
-	{/if}
-</main>
+	</main>
+{/if}
